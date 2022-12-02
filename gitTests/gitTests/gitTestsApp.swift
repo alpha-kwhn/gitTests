2a3a66329b864b462285cc575e85feae90b479ad
김응관 <kwhn9486@naver.com>
2022-12-02 10:31:32 +0900

    df

---
+++ gitTests/gitTests/gitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestsApp.swift
+//  gitTests
+//
+//  Created by 김응관 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct gitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
